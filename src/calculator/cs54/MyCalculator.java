18221b3410f597b6383873bbed70d24c6aa6e67b
Muhammad Sharaf <m.sharf47@gmail.com>
2017-10-22 22:11:24 +0200

    adding calculator

---
+++ src/calculator/cs54/MyCalculator.java
@@ -0,0 +1,225 @@
+package eg.edu.alexu.csd.oop.calculator.cs54;
+
+import eg.edu.alexu.csd.oop.calculator.Calculator;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+
+/**
+ * Calculator Class.
+ */
+public class MyCalculator implements Calculator {
+    /**
+     * current Formula Member.
+     */
+    private String currentFormula;
+    /**
+     * Holds formula History.
+     */
+    private LinkedList<String> formulas = new LinkedList<>();
+    /**
+     * Current Index Of Formulas History.
+     */
+    private int currentFormulaIndex = -1;
+    /**
+     * To Show When it's saved and not yet loaded.
+     */
+    private boolean newSave;
+
+    /**
+     * History maximum size.
+     */
+    private final int historySize = 5;
+
+    /**
+     * takes formula form user.
+     *
+     * @param s is the input
+     */
+    @Override
+    public final void input(final String s) {
+        currentFormula = s;
+        if (formulas.size() == historySize) {
+            formulas.removeFirst();
+        }
+
+        formulas.add(currentFormula);
+        currentFormulaIndex = formulas.indexOf(currentFormula);
+    }
+
+    /**
+     * takes formula.
+     *
+     * @return split formula
+     */
+    @Override
+    public final String getResult() {
+        char operator = ' ';
+        int operatorIndex = 0;
+        for (int i = 0; i < currentFormula.length(); i++) {
+            operator = currentFormula.charAt(i);
+            if (operator == '/' || operator == '*'
+                    || operator == '+' || operator == '-') {
+                operatorIndex = i;
+                break;
+            }
+        }
+        String firstTerm = currentFormula.substring(0, operatorIndex);
+        String secondTerm = currentFormula.substring(operatorIndex + 1,
+                currentFormula.length());
+
+        if (!checkFormula(firstTerm, secondTerm)) {
+            return "Invalid Formula";
+        }
+
+        return calculate(firstTerm, operator, secondTerm);
+    }
+
+    /**
+     * calculate formula result.
+     *
+     * @param firstTerm  first operand
+     * @param operator   operator
+     * @param secondTerm second operand
+     * @return calculation result
+     */
+    private String calculate(final String firstTerm,
+                             final char operator, final String secondTerm) {
+        Double value;
+        switch (operator) {
+            case '+':
+                value = Double.valueOf(firstTerm) + Double.valueOf(secondTerm);
+                break;
+            case '-':
+                value = Double.valueOf(firstTerm) - Double.valueOf(secondTerm);
+                break;
+            case '/':
+                value = Double.valueOf(firstTerm) / Double.valueOf(secondTerm);
+                break;
+            case '*':
+                value = Double.valueOf(firstTerm) * Double.valueOf(secondTerm);
+                break;
+            default:
+                return null;
+        }
+        return String.valueOf(value);
+    }
+
+    /**
+     * checks if formula is valid.
+     *
+     * @param firstTerm  first operand
+     * @param secondTerm second operand
+     * @return true if valid and false if invalid
+     */
+    private boolean checkFormula(final String firstTerm,
+                                 final String secondTerm) {
+        try {
+            if (Double.valueOf(firstTerm).isNaN()
+                    || Double.valueOf(secondTerm).isNaN()) {
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return current formula.
+     */
+    @Override
+    public final String current() {
+        if (formulas.isEmpty()) {
+            return null;
+        }
+        return currentFormula;
+    }
+
+    /**
+     * @return previous formula in history.
+     */
+    @Override
+    public final String prev() {
+        if (formulas.isEmpty()) {
+            return null;
+        }
+        if (currentFormulaIndex == -1) {
+            currentFormulaIndex = formulas.size() - 1;
+        }
+        if (currentFormulaIndex == 0) {
+            return null;
+        } else {
+            currentFormulaIndex--;
+            currentFormula = formulas.get(currentFormulaIndex);
+            return currentFormula;
+        }
+    }
+
+    /**
+     * @return next formula in history.
+     */
+    @Override
+    public final String next() {
+        if (currentFormulaIndex == -1
+                || currentFormulaIndex == formulas.size() - 1) {
+            return null;
+        } else {
+            currentFormulaIndex++;
+            currentFormula = formulas.get(currentFormulaIndex);
+            return currentFormula;
+        }
+    }
+
+    /**
+     * save history.
+     */
+    @Override
+    public final void save() {
+        newSave = true;
+        try {
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter("savedCalculations.txt"));
+            writer.write(String.valueOf(currentFormulaIndex));
+            writer.newLine();
+            for (String formula : formulas) {
+                writer.write(formula + ',');
+            }
+            writer.close();
+        } catch (IOException e) {
+            throw null;
+        }
+    }
+
+    /**
+     * load history.
+     */
+    @Override
+    public final void load() {
+        if (newSave) {
+            String[] savedFormulas = null;
+            try {
+                BufferedReader reader = new BufferedReader(
+                        new FileReader("savedCalculations.txt"));
+                currentFormulaIndex = Integer.valueOf(reader.readLine());
+                savedFormulas = reader.readLine().split(",");
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+            formulas.clear();
+            if (savedFormulas == null) {
+                currentFormula = null;
+            } else {
+                for (int i = 0; i < savedFormulas.length; i++) {
+                    formulas.add(savedFormulas[i]);
+                }
+                currentFormula = formulas.get(currentFormulaIndex);
+            }
+            newSave = false;
+        }
+    }
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,66 @@
+package sample;
+
+import eg.edu.alexu.csd.oop.calculator.cs54.MyCalculator;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    private MyCalculator calculator = new MyCalculator();
+    public Label inputFormela;
+    public Label statusLabel;
+
+    public void NumOnClick(ActionEvent e) {
+        if (inputFormela.getText() == "Invalid Formula")
+            inputFormela.setText("");
+        inputFormela.setText(inputFormela.getText() + ((Button) e.getSource()).getText());
+        statusLabel.setText("");
+    }
+
+    public void RemoveLastChar() {
+        String currentInput = inputFormela.getText();
+        int length = currentInput.length();
+        if (length > 0)
+            inputFormela.setText(currentInput.substring(0, length - 1));
+        statusLabel.setText("");
+    }
+
+    public void ClearAll (){
+        inputFormela.setText("");
+        statusLabel.setText("");
+    }
+
+    public void getResult () {
+        calculator.input(inputFormela.getText());
+        inputFormela.setText(calculator.getResult());
+        statusLabel.setText("");
+    }
+
+    public void current () {
+        inputFormela.setText(calculator.current());
+        statusLabel.setText("");
+    }
+
+    public void next () {
+        calculator.next();
+        inputFormela.setText(calculator.current());
+        statusLabel.setText("");
+    }
+
+    public void prev () {
+        calculator.prev();
+        inputFormela.setText(calculator.current());
+        statusLabel.setText("");
+    }
+
+    public void save () {
+        calculator.save();
+        statusLabel.setText("Successfully Formulas Saved");
+    }
+
+    public void load () {
+        calculator.load();
+        statusLabel.setText("Successfully Formulas Loaded");
+    }
+}
